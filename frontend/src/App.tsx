c6f2dd42e35b3ff5b0afafeb9fd1658e1e05ab52
scrp <scrappyjs666@gmail.com>
2023-10-01 16:03:06 +0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch('http://localhost:3000')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  const reset = () => {
+    setData([]);
+  };
+
+  return (
+    <>
+      <button onClick={fetchData}>Fetch Data</button>
+      {data.length > 0 && data.map((el) => <div style={{ color: 'red' }}>{el}</div>)}
+      <button onClick={reset}>Clear</button>
+    </>
+  );
+}
+
+export default App;
